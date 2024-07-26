5bd7c588a3dc39204513505069625bb9a12e631a
unknown <anmoltripathi610@gmail.com>
2024-07-26 23:28:05 +0530

    first6

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from "express"
 import cors from "cors"
 const app=express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-  }))
+
 
 app.use(express.json({limit:"16kb"}))
+const corsOptions = {
+  origin: 'https://scrapper-front-hazel.vercel.app',
+  methods:["GET","POST","PUT","DELETE"],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static("public"))
 
